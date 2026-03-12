ef592d8e167b5b72f40383c78dc71b80d249a2da
Christian Chavez <chavezchristian910@gmail.com>
2026-03-11 19:50:17 -0500

    renamed title.py to menu.py, added menu screens and navigation between them

--- lost_in_time/button.py
+++ lost_in_time/button.py
@@ -2,11 +2,13 @@ import pygame
 
 class Button:
     # Intialize button traits like position, text, and size of rect
-    def __init__ (self, text: str, pos: tuple, width: int, height: int) -> None:
+    def __init__ (self, text: str, pos: tuple, width: int, height: int, rect_color: str = "#FFFFFF") -> None:
         self.text = text
         self.rect = pygame.Rect(0, 0, width, height)
         self.rect.center = pos
+        self.rect_color = rect_color
         self.clicked = False
+        self.font = pygame.font.SysFont("Times New Roman", 80, True)
 
     # Handle mouse button click
     def handle_event(self, event: pygame.event.Event) -> None:
@@ -16,8 +18,6 @@ class Button:
     
     # Draw rect + text within rect
     def draw(self, screen: pygame.Surface) -> None:
-        pygame.draw.rect(screen, pygame.Color("#2E43FC"), self.rect)
-        font = pygame.font.SysFont("Times New Roman", 80, True)
-        button_label = font.render(self.text, True, pygame.Color("#000000"))
-        screen.blit(button_label, (self.rect.centerx - button_label.get_width() // 2, 
-                                   self.rect.centery - button_label.get_height() // 2))
+        pygame.draw.rect(screen, pygame.Color(self.rect_color), self.rect)
+        button_label = self.font.render(self.text, True, pygame.Color("#000000"))
+        screen.blit(button_label, (button_label.get_rect(center=self.rect.center)))
---
+++ lost_in_time/menu.py
@@ -0,0 +1,66 @@
+import pygame
+
+from lost_in_time.button import Button
+
+class Menu:
+    def __init__(self, screen_width: int, screen_height: int, menu: str) -> None:
+        # Initialized screen tracking, buttons and fonts for title name and header
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+        self.menu = menu
+        self.next_screen = None
+        self.buttons = []
+        self.title_font = pygame.font.SysFont("Times New Roman", 200, True, True)
+        self.header_font = pygame.font.SysFont("Times New Roman", 100, True)
+        self.background_color = "#A7A7A7"
+
+        # Creation of buttons for each menu screen
+        if self.menu == "title":
+            self.buttons.append(Button("Play", (self.screen_width // 2, 500), 180, 100, "#8DF78DFF"))
+        elif self.menu == "game_mode":
+            self.buttons.append(Button("Local", (self.screen_width // 2 - 300, 600), 220, 100, self.background_color))
+            self.buttons.append(Button("Multiplayer", (self.screen_width // 2 + 320, 600), 450, 100, self.background_color))
+            self.buttons.append(Button("Back", (self.screen_width // 2, 900), 450, 100, self.background_color))
+        elif self.menu == "level_select":
+            self.buttons.append(Button("1", (self.screen_width // 2 - 300, 600), 220, 100, self.background_color))
+            self.buttons.append(Button("2", (self.screen_width // 2 - 150, 600), 220, 100, self.background_color))
+            self.buttons.append(Button("3", (self.screen_width // 2, 600), 220, 100, self.background_color))
+            self.buttons.append(Button("4", (self.screen_width // 2 + 150, 600), 220, 100, self.background_color))
+            self.buttons.append(Button("5", (self.screen_width // 2 + 300, 600), 220, 100, self.background_color))
+            self.buttons.append(Button("Back", (self.screen_width // 2, 900), 450, 100, self.background_color))
+
+    # event in relation to button click
+    def handle_event(self, event: pygame.event.Event) -> None:
+        for button in self.buttons:
+            button.handle_event(event)
+            # Assign next_screen to allow tranistion between screen menus in relation to 
+            # button clicks
+            if button.clicked: 
+                if button.text == "Play":
+                    self.next_screen = "game_mode"
+                if button.text == "Local":
+                    self.next_screen = "level_select"
+                if button.text == "Back":
+                    self.next_screen = "back"
+                if button.text == "1":
+                    self.next_screen = "game"
+                    
+    # draw background 
+    def draw(self, screen: pygame.Surface) -> None:
+        # Draw menu design while also drawing buttons for their respective menu
+        screen.fill(pygame.Color(self.background_color))
+        game_name = self.title_font.render("Lost in Time", True, pygame.Color("#000000"))
+        screen.blit(game_name, (game_name.get_rect(center=(self.screen_width // 2, 125))))
+
+        if self.menu == "game_mode":
+            game_mode_text = self.header_font.render("Choose Game Mode", True, pygame.Color("#000000"))
+            screen.blit(game_name, (game_name.get_rect(center = (self.screen_width // 2, 125))))
+            screen.blit(game_mode_text, (game_mode_text.get_rect(center = (self.screen_width // 2, 350))))
+        elif self.menu == "level_select":
+            level_select_text = self.header_font.render("Choose Level", True, pygame.Color("#000000"))
+            screen.blit(level_select_text, (level_select_text.get_rect(center = (self.screen_width // 2, 350))))
+
+        for button in self.buttons:
+            button.draw(screen)
+
+
